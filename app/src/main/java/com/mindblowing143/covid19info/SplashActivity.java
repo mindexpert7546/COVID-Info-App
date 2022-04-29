bf97ddb4b1c2e931c7bb6314cc162d2eb092e921
mindexpert7546 <kk7615117@gmail.com>
2022-04-30 01:34:29 +0530

    Initial commit

---
+++ app/src/main/java/com/mindblowing143/covid19info/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.mindblowing143.covid19info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //hide actionBar
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        },3000);
+    }
+}
